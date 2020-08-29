3b753a26bb537e7d6d68f3bb5d48958cbb51a3cd
Khafid Prayoga <yogaengginerindo@gmail.com>
2020-08-29 06:51:39 +0000

    add secret num

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+
 use wasm_bindgen::prelude::*;
+use rand::Rng;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn random(name: String) -> String {
+  let secret_number = rand::thread_rng().gen_range(1, 101).to_string();
+  let r = String::from("Hello, ");
+  return r + &name;
 }
