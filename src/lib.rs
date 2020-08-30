c66751c04d0f1d7f8da4db78e388442dea5517d0
Khafid Prayoga <yogaengginerindo@gmail.com>
2020-08-30 07:55:32 +0000

    add sum func

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-
 use wasm_bindgen::prelude::*;
-use rand::Rng;
 
 #[wasm_bindgen]
-pub fn random(name: String) -> String {
-  let secret_number = rand::thread_rng().gen_range(1, 101).to_string();
-  let r = String::from("Hello, ");
-  return r + &name;
+pub fn sum(number: i32) -> i32{
+  return number * 100;
 }
