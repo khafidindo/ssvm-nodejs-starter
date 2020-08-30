acaf2fce71914251c722856f6cabe92e333a5ec7
Khafid Prayoga <yogaengginerindo@gmail.com>
2020-08-30 07:55:44 +0000

    add testing func

--- node/test.js
+++ node/test.js
@@ -1,2 +1,2 @@
-const { random } = require('../pkg/ssvm_nodejs_starter_lib.js');
-console.log( random("John Doe") );
+const { sum } = require('../pkg/ssvm_nodejs_starter_lib.js');
+console.log( sum(100) );
