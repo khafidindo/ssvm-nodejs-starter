3581e1c6d91ab046619da2cbd1e68362ff79d42f
Khafid Prayoga <yogaengginerindo@gmail.com>
2020-09-03 04:26:39 +0000

    edit  tester script

--- node/test.js
+++ node/test.js
@@ -1,2 +1,2 @@
-const { sum } = require('../pkg/ssvm_nodejs_starter_lib.js');
-console.log( sum(100) );
+const { calculate } = require('../pkg/ssvm_nodejs_starter_lib.js');
+console.log( calculate(9500,2) );
